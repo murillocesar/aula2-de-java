5188380ebd5b075c3a6c72a4fb448917c95718f1
murillocesar <murillocbispo@gmail.com>
2018-10-31 21:44:29 -0300

    modificaçao de java, css e html

---
+++ script.js
@@ -0,0 +1,35 @@
+// console.log('Foi!')
+
+// let texto = 'texto'
+// let numero = 1;
+// let booleano = true;
+
+// let nome = prompt('Qual seu nome?');
+// let valor = prompt('Qual valor do seu emprestimo?');
+
+// alert(`Olá ${nome}!`);
+// alert(`Você gostaria de emprestar R$${valor}, certo?`);
+
+// let pretendePagar = confirm('Você pretende pagar o seu empréstimo?');
+
+// alert(`Ok! Obrigado por nos visitar!`);
+
+let lista = ['Começo da lista', 1, 2, 3, 4, 5, 'Ola', 'Murillo', 55, true, false];
+
+let objeto = {
+    nome: 'Murillo',
+    idade: 18,
+    progamador: true,
+    namorada: 'tenho interesse em uma',
+    gatos: [
+        {
+            nome: 'claudio',
+            raça: 'SPD'
+        },
+        {
+            nome: 'Lucas',
+            raça: 'Toma Toma'
+        }
+    ]
+};
+
